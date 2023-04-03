e4c66f944a0eb8a77095c9673dbb5a0178622632
Tore Bjolseth <tbjolset@cisco.com>
2023-04-03 15:30:59 +0200

    support sending to room ids too
    look for @ in "to" field, if its not there assume room id

--- webex.js
+++ webex.js
@@ -11,11 +11,10 @@ function whoAmI(token) {
   return webex(apiUrl + 'people/me', token);
 }
 
-function sendWebexCard(token, email, text, card) {
+function sendWebexCard(token, to, text, card) {
   const url = apiUrl + 'messages';
   const body = {
     text,
-    toPersonEmail: email,
     attachments: [
       {
         contentType: "application/vnd.microsoft.card.adaptive",
@@ -23,6 +22,21 @@ function sendWebexCard(token, email, text, card) {
       },
     ]
   };
+
+  if (to.includes('@')) {
+    body.toPersonEmail = to;
+  }
+  else {
+    // typically Y2lzY29zcGFyazovL3VzL1JPT00vM2ZlMDgwMTAtZDIxZi0xMWVkLTgyY2MtMGQ2YzgwYTYxMTA1
+    // => ciscospark://us/ROOM/3fe08010-d21f-11ed-82cc-0d6c80a61105
+    let roomId = to;
+
+    // id as you get it from webex client. assume US location
+    if (to.length === 36) {
+      roomId = btoa('ciscospark://us/ROOM/' + to);
+    }
+    body.roomId = roomId;
+  }
 
   return webex(url, token, 'POST', JSON.stringify(body));
 }
